216e8eab390ebb36a72d9b5e1402e79caa1cdfa9
Tram13 <arno.vermote@hotmail.com>
2020-08-15 11:06:26 +0200

    updated Ardennen ;(

--- src/Components/Content/Ardennen.js
+++ src/Components/Content/Ardennen.js
@@ -15,17 +15,15 @@ class Ardennen extends React.Component {
                     <span className="card-title">Ardennen September 2020 - FAQ</span>
                     <p>
                         <h6 className="underline semi-bold">Datum?</h6>
-                        11-13 september
+                        11-13 september.
                         <h6 className="underline semi-bold">Vervoer?</h6>
-                        Auto van Jentl, Axel en Arno (en Brent Kappa)
+                        Auto van Axel en Arno.
                         <h6 className="underline semi-bold">En waar verblijven we dan exact?</h6>
                         <a href="https://www.auborddeleau.net/">Hier.</a>
-                        <h6 className="underline semi-bold">Waarom is het niet meer 4-6 september?</h6>
-                        Dan kunnen Axel en Jentl niet mee.
                         <h6 className="underline semi-bold">Wat doen we dan met Chloë haar concert?</h6>
                         Welk concert?
                         <h6 className="underline semi-bold">Wat met Covid-19?</h6>
-                        Afwachten wat veiligheidsraad zal beslissen.
+                        Fuck jullie allemaal.
                     </p>
                 </div>
             </AbstractCard>
